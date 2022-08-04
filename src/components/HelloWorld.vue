f60c83983c073ca1b140ccc52ef46a0bc7ae4f4c
jfkeci <ja.lip132@gmail.com>
2022-08-04 07:42:52 +0200

    chore: rebuild project

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,9 +7,6 @@
 <script>
 export default {
   name: "HelloWorld",
-  props: {
-    msg: String,
-  },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-import { createApp } from "vue";
+import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-
-import Vue from "vue";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
@@ -11,4 +12,9 @@ import "bootstrap-vue/dist/bootstrap-vue.css";
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 
-createApp(App).use(router).mount("#app");
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
