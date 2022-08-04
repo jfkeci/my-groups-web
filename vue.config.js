2c1d264a91d92d09c9edc5fb0c29cbb49aa96d95
jfkeci <ja.lip132@gmail.com>
2022-08-04 07:48:15 +0200

    chore: fix public path

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: window.location.origin.includes("localhost")
-    ? "/"
-    : "/my-groups-web/",
+  publicPath: "/my-groups-web/",
 });
