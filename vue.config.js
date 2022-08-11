9c4e89c344ddc5d69bf090fda644aac44dd10947
jfkeci <ja.lip132@gmail.com>
2022-08-11 17:52:49 +0200

    chore: add devserver to vue config

--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,7 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.VUE_APP_PUBLIC_PATH ?? "/",
+  devServer: {
+    proxy: process.env.VUE_APP_API_BASE_URL,
+  },
 });
