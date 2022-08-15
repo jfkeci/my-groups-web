e38cd066d4b180346f3b135b398af62ecfbc837f
jfkeci <ja.lip132@gmail.com>
2022-08-15 21:12:06 +0200

    chore: get user reducer method

---
+++ src/store/resources/users/users.reducers.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+export const getUser = async ({ commit }, userId) => {
+  try {
+    let user;
+
+    const res = await axios.get(`/user/${userId}`);
+
+    if (res.status == 200 && res.data) {
+      user = res.data;
+    } else {
+      user = null;
+    }
+
+    return user;
+  } catch (err) {
+    console.log("registerUser.ERR", err);
+    switch (err.response.status) {
+      case 400:
+        commit("setMessage", {
+          text: err.response.data.message,
+          type: "danger",
+        });
+        break;
+      case 404:
+        console.log(err.response);
+        commit("setMessage", {
+          text: err.response.data.message,
+          type: "danger",
+        });
+        break;
+      default:
+        commit("setMessage", {
+          text: "Something went wrong",
+          type: "danger",
+        });
+        break;
+    }
+  }
+};
