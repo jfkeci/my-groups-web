092a7dbf122e3e0275e0024b94c5beaeca32c7f8
jfkeci <ja.lip132@gmail.com>
2022-09-12 17:06:42 +0200

    chore: add and implement intro messages with i18n

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,24 @@
 <template>
   <div class="home">
-    <b-jumbotron header="My Groups" lead="Find your community"> </b-jumbotron>
+    <b-jumbotron :header="$t('appName')" :lead="$t('findYourCommunity')">
+    </b-jumbotron>
     <div v-if="!isLoggedIn">
       <b-card class="mt-2" header="">
-        <h4>Welcome to MyGroups</h4>
+        <h4>{{ $t("welcomeTo") }} {{ $t("appName") }}</h4>
         <p>
-          Please log in to your account. Then you can create your own community,
-          or ask someone to add you to their community. If you are already a
-          part of a community you can se community content
+          {{ $t("homeMessage") }}
         </p>
         <br />
-        <b> <p>Start now</p></b>
+        <b>
+          <p>{{ $t("startNow") }}</p>
+        </b>
         <br />
         <b-button block variant="success" size="sm" to="/login">
-          Login
+          {{ $t("Login") }}
         </b-button>
 
         <b-button block variant="success" size="sm" to="/register">
-          Register
+          {{ $t("Register") }}
         </b-button>
       </b-card>
     </div>
